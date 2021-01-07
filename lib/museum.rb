0bf4fe0b5dd5ee3f22eae86d71c208effe6cec4b
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-01-07 10:41:51 -0700

    Complete patrons by interest

--- lib/museum.rb
+++ lib/museum.rb
@@ -17,7 +17,23 @@ class Museum
     @exhibits.find_all do |exhibit|
       patron.interests.include?(exhibit.name)
     end
+  end
 
+  def admit(patron)
+    @patrons << patron
+  end
+
+  def patrons_by_exhibit_interests
+    grouped = Hash.new
+    @exhibits.each do |exhibit|
+      grouped[exhibit] = []
+      patrons.each do |patron|
+        if patron.interests.include?(exhibit.name)
+          grouped[exhibit] << patron
+        end
+      end
+    end
+    grouped
   end
 
 end
